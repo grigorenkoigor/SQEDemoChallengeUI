7cf01528e0bf991d1dc5590c86085314eeebca61
Igor <grigorenko.igor1994@gmail.com>
2022-09-13 12:42:52 +0200

    fix nullPointerException and some locators

--- src/main/java/com/ui/PizzaOrderFormPage.java
+++ src/main/java/com/ui/PizzaOrderFormPage.java
@@ -7,10 +7,14 @@ public class PizzaOrderFormPage {
 
     WebDriver driver;
 
+    public PizzaOrderFormPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
     By pizzaDropdownField = By.id("pizza1Pizza");
     By toppings1pizzaDropdownField = By.className("toppings1");
     By toppings2pizzaDropdownField = By.className("toppings2");
-    By quantityInputField = By.className("pizza1Qty");
+    By quantityInputField = By.cssSelector("#pizza1 #pizza1Qty");
     By costInputField = By.className("pizza1Cost");
 
     By nameInputField = By.id("name");
@@ -22,26 +26,26 @@ public class PizzaOrderFormPage {
     By placeOrderButton = By.id("placeOrder");
     By resetButton = By.id("reset");
 
-    public PizzaOrderFormPage(){
-        this.driver = driver;
-    }
-
     public PizzaOrderFormPage setNameField(String name){
+        driver.findElement(nameInputField).clear();
         driver.findElement(nameInputField).sendKeys(name);
         return this;
     }
 
     public PizzaOrderFormPage setEmailField(String email){
+        driver.findElement(emailInputField).clear();
         driver.findElement(emailInputField).sendKeys(email);
         return this;
     }
 
     public PizzaOrderFormPage setPhoneField(String phone){
+        driver.findElement(phoneInputField).clear();
         driver.findElement(phoneInputField).sendKeys(phone);
         return this;
     }
 
     public PizzaOrderFormPage setQuantityField(int quantity){
+        driver.findElement(quantityInputField).clear();
         driver.findElement(quantityInputField).sendKeys(String.valueOf(quantity));
         return this;
     }
